b177760475c14ddf6885e6592217b412b3b5400a
vaceslavkameronov <slka@mail.ru>
2023-01-31 20:39:00 +0400

    Добавлены комментарии

--- src/Main.java
+++ src/Main.java
@@ -7,22 +7,22 @@ public class Main {
 
     public static void task1() {
         System.out.println("Задача 1");
-        String firstName = "Ivan";
-        String middleName = "Ivanovich";
-        String lastName = "Ivanov";
-        String fullName = lastName + " " + firstName + " " + middleName;
+        String firstName = "Ivan"; //инициализация "имя"
+        String middleName = "Ivanovich"; //инициализация "отчество"
+        String lastName = "Ivanov"; //инициализация "фамилия"
+        String fullName = lastName + " " + firstName + " " + middleName; //создание строки для хранения ФИО в формате Фамилия Имя Отчество
         System.out.println("ФИО сотрудника - " + fullName);
 
     }
     public static void task2() {
         System.out.println("Задача 2");
-        String fullName = "Ivanov Ivan Ivanovich";
-        System.out.println("Данные ФИО сотрудника для заполнения отчета  - " + fullName.toUpperCase());
+        String fullName = "Ivanov Ivan Ivanovich"; //инициализация ФИО сотрудника
+        System.out.println("Данные ФИО сотрудника для заполнения отчета  - " + fullName.toUpperCase()); // вывод результата с использованием метода преобразования всех знаков в верхний регистр
     }
     public static void task3() {
         System.out.println("Задача 3");
-        String fullName = "Иванов Семён Семёнович";
-        fullName = fullName.replace("ё", "е");
+        String fullName = "Иванов Семён Семёнович"; //инициализация ФИО сотрудника
+        fullName = fullName.replace("ё", "е"); //замена буквы "ё" на "ё"
         System.out.println("Данные ФИО сотрудника - " + fullName);
     }
 }
